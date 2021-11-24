f254bd3fb2f7c030fe75fe06da0ac17e764d89f6
Opilane TTHK <opilane@tthk.ee>
2021-11-24 09:45:21 +0200

    week13Test

---
+++ Week13-Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Week13_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string newPath = @"C:\Users\opilane\samples\";
+            Console.WriteLine("Enter your file name: ");
+            string kasutajakaust = Console.ReadLine();
+
+            string newdirectory = $@"{newPath}\{kasutajakaust}";
+
+
+            bool directoryExists = Directory.Exists(newdirectory);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{kasutajakaust} already exists in samples");
+            }
+            else
+            {
+
+                Directory.CreateDirectory(newdirectory);
+                Console.WriteLine($"directory {kasutajakaust} has been created");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ülesanne4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ülesanne4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
